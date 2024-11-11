537782d4ad85a0747970b4a794660b55452017e5
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2024-11-11 20:55:07 +0300

    Fix bugs

--- Calculator.AlifurkanKaraman/Calculator/Program.cs
+++ Calculator.AlifurkanKaraman/Calculator/Program.cs
@@ -6,14 +6,10 @@ class Program
     static void Main(string[] args)
     {
         bool endApp = false;
-        
-
         Calculator calculator = new Calculator();
-
         while (!endApp)
         {
             double result = 0;
-
             Console.Clear();
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
@@ -24,12 +20,11 @@ class Program
             Console.WriteLine("\t4 - Exit");
             Console.Write("Your choice: ");
             string? choice = Console.ReadLine();
-
             switch (choice)
             {
                 case "1":
-                    double cleanNum1 = GetNumberFromUser(calculator, "Type a number for num1, or enter 'p' to use a previous result: ");
-                    double cleanNum2 = GetNumberFromUser(calculator, "Type a number for num2, enter 'p' to use a previous result or press any key for single calculations: ");
+                    double cleanNum1 = GetNumberFromUser(calculator, "Type a number for the first number, or enter 'p' to use a previous result: ");
+                    double cleanNum2 = GetNumberFromUser(calculator, "Type a number for the second number, enter 'p' to use a previous result or press enter for single calculations: ");
                     Console.WriteLine("Choose an operator:");
                     // For single - operand calculation
                     if (double.IsNaN(cleanNum2))
@@ -48,10 +43,8 @@ class Program
                         Console.WriteLine("\td - Divide");
                         Console.WriteLine("\tp - Power");
                     }
-                    
                     Console.Write("Your option? ");
                     string? op = Console.ReadLine();
-
                     if (op == null || !Regex.IsMatch(op, "[a|s|m|d|p|r|x|q|w|t]"))
                     {
                         Console.WriteLine("Error: Unrecognized input.");
@@ -67,33 +60,28 @@ class Program
                             }
                             else
                             {
-                                Console.WriteLine("Your result: {0:0.##}\n", result);
+                                Console.WriteLine($"Your result: {result:0.##}\n");
                             }
                         }
                         catch (Exception e)
                         {
-                            Console.WriteLine("An exception occurred trying to do the math.\n - Details: " + e.Message);
+                            Console.WriteLine($"An exception occurred trying to do the math.\n - Details: {e.Message}");
                         }
                     }
                     break;
-
                 case "2":
                     calculator.ShowPreviousResults();
                     break;
-
                 case "3":
                     calculator.ClearPreviousCalculations();
                     break;
-
                 case "4":
                     endApp = true;
                     break;
-
                 default:
                     Console.WriteLine("Invalid choice, please try again.");
                     break;
             }
-
             Console.WriteLine("------------------------\n");
             if (!endApp)
             {
@@ -101,9 +89,8 @@ class Program
                 Console.ReadLine();
             }
         }
-
         calculator.Finish();
-        Console.WriteLine("Total calculator usage: {0} times.", calculator.GetUsageCount());
+        Console.WriteLine($"Total calculator usage: {calculator.GetUsageCount()} times.");
     }
 
     static double GetNumberFromUser(Calculator calculator, string prompt)
@@ -111,7 +98,6 @@ class Program
         Console.Write(prompt);
         string? input = Console.ReadLine();
         double number;
-
         if (input == "p")
         {
             if (calculator.GetPreviousCalculations().Count > 0)
@@ -153,17 +139,15 @@ class Program
                 input = Console.ReadLine();
             }
         }
-        else if (string.IsNullOrEmpty(input))
+        else if (string.IsNullOrEmpty(input) && !prompt.Contains("first"))
         {
             return double.NaN;
         }
-
         while (!double.TryParse(input, out number))
         {
             Console.Write("This is not valid input. Please enter a numeric value: ");
             input = Console.ReadLine();
         }
-
         return number;
     }
 }
